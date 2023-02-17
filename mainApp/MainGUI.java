493667ff271cda0ea6a5f63f6bda66c57db22dea
BennyDubz <benjamin.r.williams.25@dartmouth.edu>
2023-02-16 21:06:48 -0500

    possible X11 display fix

--- mainApp/MainGUI.java
+++ mainApp/MainGUI.java
@@ -290,6 +290,7 @@ public class MainGUI extends JFrame {
     }
 
     public static void main(String[] args) throws IOException {
+        System.setProperty("java.awt.headless", "true");
 //        BufferedImage emptyMapImage = ImageIO.read(new File("Assets/emptyMountain.png"));
 //        //BufferedImage emptyMapImage = ImageIO.read(new File(MainGUI.class.getResource("Assets/emptyMountain.png").getPath()));
 //        BufferedImage fullMapImage = ImageIO.read(new File("Assets/original.png"));
